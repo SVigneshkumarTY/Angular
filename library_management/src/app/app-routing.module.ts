8550a3c6db43df84be020ef188712e23638badc9
praveenng888 <praveenng88@gmail.com>
2019-10-24 14:52:16 +0530

    Angular project with all necessary components

--- library_management/src/app/app-routing.module.ts
+++ library_management/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+import { ViewUsersComponent } from './view-users/view-users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'viewusers',component:ViewUsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- library_management/src/app/app.module.ts
+++ library_management/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { StudentComponent } from './student/student.component';
+import { LibrarianComponent } from './librarian/librarian.component';
+import { AdminComponent } from './admin/admin.component';
+import { ViewUsersComponent } from './view-users/view-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    StudentComponent,
+    LibrarianComponent,
+    AdminComponent,
+    ViewUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
